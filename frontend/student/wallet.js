1bb2cb1d3d4d077fb578d44ba54b9fbeef46ce71
Wutthisak Boonkan <wutthisak2548@gmail.com>
2026-04-25 00:14:03 +0700

    fix number only in back_account

--- frontend/student/wallet.js
+++ frontend/student/wallet.js
@@ -47,9 +47,6 @@ const confirmWithdrawBtn = document.getElementById('confirmWithdrawBtn');
 const bankName = document.getElementById('bankName');
 const accountNumber = document.getElementById('accountNumber');
 
-accountNumber.addEventListener('input', () => {
-  accountNumber.value = accountNumber.value.replace(/\D/g, '').slice(0, 10);
-});
 
 function formatCurrency(amount) {
   return `฿${Number(amount).toLocaleString('en-US', {
