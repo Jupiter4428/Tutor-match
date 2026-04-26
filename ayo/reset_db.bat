f1942a451f0d92d95962982d41ac7bd49d5ccbd8
Wutthisak Boonkan <wutthisak2548@gmail.com>
2026-04-26 20:23:37 +0700

    set up database

--- ayo/reset_db.bat
+++ ayo/reset_db.bat
@@ -1,4 +1,4 @@
 @echo off
-"C:\Program Files\MySQL\MySQL Server 9.6\bin\mysql.exe" --default-character-set=utf8mb4 -u root -p0000 < "c:\Users\U S E R\Desktop\Project\project\database\schema.sql"
-"C:\Program Files\MySQL\MySQL Server 9.6\bin\mysql.exe" --default-character-set=utf8mb4 -u root -p0000 tutor_match < "c:\Users\U S E R\Desktop\Project\project\database\seed.sql"
+"C:\Program Files\MySQL\MySQL Server 9.6\bin\mysql.exe" --default-character-set=utf8mb4 -u root -p1234 < "c:\Users\U S E R\Desktop\Project\project\database\schema.sql"
+"C:\Program Files\MySQL\MySQL Server 9.6\bin\mysql.exe" --default-character-set=utf8mb4 -u root -p1234 tutor_match < "c:\Users\U S E R\Desktop\Project\project\database\seed.sql"
 pause
--- ayo/setup_db.bat
+++ ayo/setup_db.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files\MySQL\MySQL Server 9.6\bin\mysql.exe" --default-character-set=utf8mb4 -u root -p0000 < "c:\Users\U S E R\Desktop\Project\project\database\schema.sql"
+"C:\Program Files\MySQL\MySQL Server 9.6\bin\mysql.exe" --default-character-set=utf8mb4 -u root -p1234 < "c:\Users\U S E R\Desktop\Project\project\database\schema.sql"
 pause
